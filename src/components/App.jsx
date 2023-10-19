27b95c703222b092f9dd421d9e24c44d45926df9
YuriyOhii <uaogiy@gmail.com>
2023-10-19 12:51:21 +0300

    adds old phonebooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,83 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm';
+import { Filter } from './Filter';
+import { ContactList } from './ContactList';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      const contacts = JSON.parse(savedContacts);
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(_, pS) {
+    const { contacts } = this.state;
+    if (pS.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
+  checkContactName = values => {
+   
+    const isNameInPhonebook = this.state.contacts.find(
+      ({ name }) => name === values.name
+    );
+    return isNameInPhonebook;
+  };
+
+  handleSubmit = values => {
+    if (this.checkContactName(values))
+      return alert(`${values.name} already is in the PhoneBook`);
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { id: nanoid(), ...values }],
+    }));
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({ filter: target.value });
+  };
+
+  deleteContact = ({ target }) => {
+    const updatedContacts = this.state.contacts.filter(
+      ({ id }) => id !== target.value
+    );
+    this.setState({ contacts: updatedContacts });
+  };
+
+  getFilteredContacts = () => {
+    const normalizedValue = this.state.filter.toLowerCase();
+    return this.state.contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedValue)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm handleSubmit={this.handleSubmit} />
+
+        <h2>Contacts</h2>
+        <Filter value={filter} handleChange={this.handleChange} />
+        <ContactList
+          contacts={this.getFilteredContacts()}
+          onChange={this.deleteContact}
+        />
+      </>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from 'components/GlobalStyles';
+
+const theme = {
+  colors: {
+    grey: '#212121',
+    red: 'red',
+    white: 'white',
+    yellow: 'yellow',
+  },
+  spacing: value => `${value * 4}px`,
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>
 );
