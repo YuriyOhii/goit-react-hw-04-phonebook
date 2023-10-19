c2bdc38bc860b9d17513b4e855756f0070a6a86a
YuriyOhii <uaogiy@gmail.com>
2023-10-19 12:57:18 +0300

    adds layout

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from 'components/GlobalStyles';
+import { Layout } from 'components/Layout.styled';
 
 const theme = {
   colors: {
@@ -17,8 +18,10 @@ const theme = {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
-      <GlobalStyles />
+      <Layout>
+        <App />
+        <GlobalStyles />
+      </Layout>
     </ThemeProvider>
   </React.StrictMode>
 );
